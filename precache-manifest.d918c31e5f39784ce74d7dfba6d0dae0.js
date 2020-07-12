5a0c9b5bd18fb266c60ef20f15baaff7d40a9deb
Oksana Tkachenko <coatceatl@gmail.com>
2020-07-12 21:37:33 +0300

    Updates

---
+++ precache-manifest.d918c31e5f39784ce74d7dfba6d0dae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0ccedd07cb124dad5603f488f35144",
+    "url": "/flags-animation/index.html"
+  },
+  {
+    "revision": "78d988114620ec3d1f60",
+    "url": "/flags-animation/static/css/main.a4e577cf.chunk.css"
+  },
+  {
+    "revision": "f138e40139a920547dba",
+    "url": "/flags-animation/static/js/2.f3241a7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flags-animation/static/js/2.f3241a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d988114620ec3d1f60",
+    "url": "/flags-animation/static/js/main.39bf54fb.chunk.js"
+  },
+  {
+    "revision": "b580fd9354fa43c086c3",
+    "url": "/flags-animation/static/js/runtime-main.623ed061.js"
+  }
+]);
